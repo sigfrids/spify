855f743fbf343d6c05c8ac5efa1ec859cc72c873
Fergal Duggan <sligseach@spotify.com>
2015-06-03 14:48:51 +0200

    Made a bigger attachment

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ app.post('/play', function(req, res) {
           }
           else {
             var spifyBody = '{"attachments": [ {';
-            spifyBody += '"text": "<' + results[0].preview_url + '|' + results[0].name + '>"';
+            spifyBody += '"pretext": "Spify found"';
+            spifyBody += '"title": "' + results[0].name + '"';
+            spifyBody += '"title_link": "' + results[0].preview_url + '"';
+            spifyBody += '"text": "' + results[0].artists[0].name + '"';
+            //spifyBody += '"text": "<' + results[0].preview_url + '|' + results[0].name + '>"';
+            spifyBody += '"thumb_url: "' + results[0].album.images[2].url + '"';
+            spifyBody += '"color": "#2ebd59"';
             spifyBody += '}]}';
 
             //body: '{"attachments": [ {
