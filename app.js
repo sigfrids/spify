37f82ac72a8031928d9f91f7e3ae908b1506909b
Karl Sigfrids <sigfrids@spotify.com>
2015-06-03 14:15:05 +0200

    get rid of crap

--- app.js
+++ app.js
@@ -20,20 +20,6 @@ app.use(bodyParser.urlencoded({
 
 var spifyBotUrl = "https://hooks.slack.com/services/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";
 
-/*app.get('/', function(req, res) {
-  if (spotifyApi.getAccessToken()) {
-    return res.send('You are logged in.');
-  }
-  return res.send('<a href="/authorise">Authorise</a>');
-});*/
-
-/*app.get('/authorise', function(req, res) {
-  var scopes = ['playlist-modify-public', 'playlist-modify-private'];
-  var state  = new Date().getTime();
-  var authoriseURL = spotifyApi.createAuthorizeURL(scopes, state);
-  res.redirect(authoriseURL);
-});*/
-
 app.post('/play', function(req, res) {
   if (req.body.token !== process.env.SLACK_TOKEN) {
     return res.status(500).send('Sea surf!');
@@ -59,63 +45,6 @@ app.post('/play', function(req, res) {
           return res.send(err.message);
         });
 });
-
-/*app.get('/callback', function(req, res) {
-  spotifyApi.authorizationCodeGrant(req.query.code)
-    .then(function(data) {
-      spotifyApi.setAccessToken(data.body['access_token']);
-      spotifyApi.setRefreshToken(data.body['refresh_token']);
-      return res.redirect('/');
-    }, function(err) {
-      return res.send(err);
-    });
-});*/
-
-/*app.use('/play', function(req, res, next) {
-  if (req.body.token !== process.env.SLACK_TOKEN) {
-    return res.status(500).send('Sea surf!');
-  }
-  next();
-});*/
-
-/*var makeSlackResponse = function(trackName, preview, uri) {
-  var spotifyLink = "https://open.spotify.com/track/";
-  var textResponse = 'Matched ' + '"' + trackName + '": ' + '\n' +
-      preview+ '\n' +
-      uri;
-  return {'text' : textResponse};
-};*/
-
-/*app.post('/play', function(req, res) {
-  spotifyApi.refreshAccessToken()
-    .then(function(data) {
-      spotifyApi.searchTracks(req.body.text)
-        .then(function(data) {
-          var results = data.body.tracks.items;
-          if (results.length === 0) {
-            return res.send('Could not match a track lol ¯|_(ツ)_/¯');
-          }
-          else {
-            return request.post({
-              url: spifyBotUrl,
-              body: '{"text": 
-              "<' + results[0].preview_url + '|' + results[0].name + '>"'
-              //+ results[0].almum.image[0]
-              + '}'
-              //body: '{"attachments": [ { "fallback": "Network traffic (kb/s): How does this look? @slack-ops - Sent by Julie Dodd - https://datadog.com/path/to/event", "title": "Network traffic (kb/s)", "title_link": "https://datadog.com/path/to/event", "text": "How does this look? @slack-ops - Sent by Julie Dodd", "image_url": "https://datadoghq.com/snapshot/path/to/snapshot.png", "color": "#764FA5"}]}'
-            }, function (error, response, body){
-              if (error) {
-                return res.send('Unable to publish to channel.');
-              }
-            });
-          }
-        }, function(err) {
-          return res.send(err.message);
-        });
-    }, function(err) {
-      return res.send('Could not refresh access token, try reauthenticating.');
-    });
-});*/
 
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'));
