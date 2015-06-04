f9c969ace6c5d4a4426524fd267938cc201887f8
Karl Sigfrids <sigfrids@spotify.com>
2015-06-04 11:17:46 +0200

    include query in result

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/play', function(req, res) {
           }
           else {
             var spifyBody = '{"attachments": [ {';
-            spifyBody += '"pretext": "spify jumba gumba to", ';
+            spifyBody += '"pretext": "' + req.body.text + '", ';
             spifyBody += '"title": "' + results[0].name + '", ';
             spifyBody += '"title_link": "' + results[0].preview_url + '", ';
             spifyBody += '"text": "' + 'Artist: ' + results[0].artists[0].name + '\\nAlbum: ' + results[0].album.name + '", ';
